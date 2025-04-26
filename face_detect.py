f1c71b47cdac88d8f8c2dd1a2c7baf8b578e8939
WimukthiPWijerathne <wimukthiwijerathne@gmail.com>
2025-04-26 11:47:57 +0530

    built a face recognition system

--- face_detect.py
+++ face_detect.py
@@ -14,7 +14,7 @@ gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
 haar_cascade = cv.CascadeClassifier('haar_face.xml')
 
-faces_rect = haar_cascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=3)
+faces_rect = haar_cascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=1)
 
 
 print(f'Number of faces found = {len(faces_rect)}')
@@ -22,7 +22,7 @@ print(f'Number of faces found = {len(faces_rect)}')
 
 for (x,y,w,h) in faces_rect:
     cv.rectangle(img,(x,y),(x+w,y+h),(0,255,0),1)
-    cv.putText(img,'Face Detected',(x,y-10),cv.FONT_HERSHEY_COMPLEX,0.7,(0,255,0),2)
+    # cv.putText(img,'Face Detected',(x,y-10),cv.FONT_HERSHEY_COMPLEX,0.7,(0,255,0),2)
 
 cv.imshow('Detected Faces',img)
 cv.waitKey(0)
